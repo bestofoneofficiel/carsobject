ab91d54a5759cf251fbf7e086cb7ba3974b7ef31
Nicolas <n.meunier836@gmail.com>
2019-04-17 11:10:09 +0200

    le message de caca

--- Debut/index.php
+++ Debut/index.php
@@ -2,16 +2,17 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Document sans titre</title>
+<title>titre</title>
 </head>
 
 <body>
 	<?php 
 	// INCLUSION
 		include('./include/include.php'); 
-
-/*	
-	// SANS CONSTRCTEUR
+	
+	
+	echo '<h2>SANS CONSTRUCTEUR</h2>';
+	// SANS CONSTRUCTEUR
 		// Creer l'objet
 		$animalSC = new AnimalSansConstruct;
 
@@ -24,6 +25,8 @@
 	
 		$animalSC->afficheMonPoids();
 	
+	
+	echo '<h2>AVEC CONSTRUCTEUR</h2>';
 	// AVEC CONSTRUCTEUR
 		// Creer l'objet et definir le poids
 		$animalAC = new AnimalAvecConstruct(40, 120);
@@ -32,12 +35,14 @@
 		var_dump($animalAC);
 	
 	
+	echo '<h2>STATIQUE</h2>';
 	// STATIQUE
 		// Afficher l'animal statique
 		echo 'AnimalStatic : ' . AnimalStatic::$_poids . '<br/>';
 		echo 'AnimalStatic : ' . AnimalStatic::getPoids();
 	
 	
+	echo '<h2>HERITAGE</h2>';
 	// HERITAGE
 		// créer un objet enfant
 		$chien = new Chien(40, 120, 'golden');
@@ -48,23 +53,42 @@
 		// Affichier la fonction du parent depuis l'enfant
 		echo 'poids du iench : '; $chien->donneMoiLePoids(); echo '<br/>';
 	
-	// CLASSE ABSTRAITES
+	
+	echo '<h2>CLASSES ABSTRAITES</h2>';
+	// CLASSES ABSTRAITES
 		// Animal ne peut pas être instancier car il est abstrait
 		// $animalAbstrait = new Animal(10, 110);
 
-		// 
+
 		$chien1 = new Chien(5, 60, 'cocker');
 		var_dump($chien1);
 	
-*/	
-		// 
+
+	echo '<h2>EXCEPTIONS</h2>';
+	// EXCEPTIONS
 		$lapin1 = new Lapin(34, 'vrai', 'Lapin');
 		$lapin1->manger();
 		var_dump($lapin1);
-		$rodent = new Lapin(45, 'Faux', 'vache');
-		var_dump($rodent);
-	
 		
+		$rodent = new Lapin(45, 'Faux', 'vache');
+		
+	
+	echo '<h2>Méthodes magiques</h2>';
+	// __set()
+		// Attribut public
+		$chien1->sale = 'oui';
+	
+		// Atrribut privé
+		$chien1->_race = 90;
+	
+		// Attribut innexistant
+		$chien1->age = 12;
+	
+	// __get()
+		// Attribut public
+		$chien1->_race;
+		
+	echo '<br /><br /><br /><br /><br />';
 	?>
 </body>
 </html>
